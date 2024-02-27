df1f64df345593bcc3b1894ecb451c28bff062f5
Lorena <lorelore9499@gmail.com>
2024-02-27 10:06:37 +0000

    backend ready

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,13 +1,13 @@
 const router = require('express').Router()
 
 const { getAllUsers, getOneUser, getOwnProfile, createUser, updateUser, deleteUser } = require('../controllers/user')
-//const { checkAuth, checkAdmin } = require('../utils/check')
+const { checkAuth, checkAdmin } = require('../utils/check')
 
-router.get('/', getAllUsers)
-router.get('/profile', getOwnProfile)
-router.get('/:id', getOneUser)
-router.post('/', createUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.get('/', checkAuth, checkAdmin, getAllUsers)
+router.get('/profile', checkAuth, getOwnProfile)
+router.get('/:id', checkAuth, checkAdmin, getOneUser)
+router.post('/', checkAuth, checkAdmin, createUser)
+router.put('/:id', checkAuth, checkAdmin, updateUser)
+router.delete('/:id', checkAuth, checkAdmin, deleteUser)
 
 module.exports = router
--- api/utils/check.js
+++ api/utils/check.js
@@ -1,33 +1,34 @@
-//no está funcionando correctamente
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-// const User = require("../models/user");
-// const jwt = require("jsonwebtoken");
+const checkAuth = (req, res, next) => {
+  if (!req.headers.authorization)
+    return res.status(401).send("Token not found");
 
-// const checkAuth = (req, res, next) => {
-//     if (!req.headers.authorization) return res.status(401).send("Token not found");
-  
-//     jwt.verify(req.headers.authorization, process.env.JWT_SECRET,
-//       async (err, result) => {
-//         if (err) return res.status(401).send("Token not valid");
-  
-//         const user = await User.findOne({ where: { email: result.email } });
-//         if (!user) return res.status(401).send("Token not valid");
-  
-//         res.locals.user = user;
-//         next();
-//       }
-//     );
-//   }
+  jwt.verify(
+    req.headers.authorization,
+    process.env.JWT_SECRET,
+    async (err, result) => {
+      if (err) return res.status(401).send("Token not valid");
 
-//   const checkAdmin = (req, res, next) => {
-//     if ( res.locals.user.role !== 'admin' ) {
-//       return res.status(401).send('User not authorized')
-//     } else {
-//       next()
-//     }
-//   }
-  
-//   module.exports = {
-//     checkAuth,
-//     checkAdmin
-//   }
+      const user = await User.findOne({ where: { email: result.email } });
+      if (!user) return res.status(401).send("Token not valid");
+
+      res.locals.user = user;
+      next();
+    }
+  );
+};
+
+const checkAdmin = (req, res, next) => {
+  if (res.locals.user.role !== "admin") {
+    return res.status(401).send("User not authorized");
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  checkAuth,
+  checkAdmin,
+};
